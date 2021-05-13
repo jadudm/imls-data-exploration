29db1809032e722880784ba60626dc4803d46dd1
Matt Jadud <matthew.jadud@gsa.gov>
2021-05-13 14:15:55 -0400

    Broke out the key.
    To make sure I can't accidentally commit the key, I put it in `key.js`,
    and made that an ignored file.
    
    The readme has the info for recreating the file.
    
    :shrug:
    
    Yes, it is exploratory, but I know I'll commit a key. It's a superpower.
    And, yes, I know we have gitleaks running. But still. :)

--- README.md
+++ README.md
@@ -16,3 +16,15 @@ make html
 ## Authorization
 
 The `utils.js` file must be configured with the appropriate api.data.gov key.
+
+## API Key
+
+Create a (git ignored) `key.js`:
+
+```
+const api_key = "YOURKEYHERE"
+
+module.exports = {
+    api_key
+  };
+```
--- utils.js
+++ utils.js
@@ -1,8 +1,6 @@
+const { api_key } = require('./key.js');
 
 const base_url = 'https://api.data.gov/TEST/10x-imls/v1';
-
-// TODO: public key
-const key = 'yourkeyhere';
 
 async function queryServer(query) {
   const options = {
@@ -10,7 +8,7 @@ async function queryServer(query) {
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({ query })
   };
-  var result = await fetch(`${base_url}/graphql/?api_key=${key}`, options);
+  var result = await fetch(`${base_url}/graphql/?api_key=${api_key}`, options);
   return result.json();
 }
 
