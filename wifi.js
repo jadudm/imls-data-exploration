a883bbc38005f82419ab88e2d96fa95c49bbea75
James Tranovich <james.tranovich@gsa.gov>
2021-05-13 17:05:01 -0700

    get session data.

--- wifi.js
+++ wifi.js
@@ -3,12 +3,17 @@ const { queryServer } = require('./utils.js');
 
 const wifi_count = 1000;
 
-function getWifiData(library) {
+// TODO: wifi minutes count
+// TODO: filter out devices (less than 10 minutes, ...)
+
+function getSessionData(library, session, page) {
   return `
 {
   items {
     wifi_v1(limit: ${wifi_count},
-            filter: { fcfs_seq_id: { _eq: "${library}" } },
+            page: ${page},
+            filter: { fcfs_seq_id: { _eq: "${library}" },
+                      session_id: { _eq: "${session}" } },
             sort: ["servertime"]) {
       id
       session_id
@@ -16,6 +21,37 @@ function getWifiData(library) {
       manufacturer_index
       patron_index
       localtime
+      servertime
+    }
+  }
+}
+`;
+}
+
+function getSessionStart(library, session) {
+  return `
+{
+  items {
+    wifi_v1(limit: 1,
+            filter: { fcfs_seq_id: { _eq: "${library}" },
+                      session_id: { _eq: "${session}" } },
+            sort: ["servertime"]) {
+      servertime
+    }
+  }
+}
+`;
+}
+
+function getCurrentSession(library) {
+  return `
+{
+  items {
+    wifi_v1(limit: 1,
+            filter: { fcfs_seq_id: { _eq: "${library}" } },
+            sort: ["-servertime"]) {
+      session_id
+      servertime
     }
   }
 }
@@ -26,8 +62,26 @@ async function chartLibraryInformation(library) {
   var el = document.getElementById('library-chart');
   el.innerHTML = '';
 
-  var data = await queryServer(getWifiData(library));
-  var wifi = data.data.items.wifi_v1;
+  var currentSessionData = await queryServer(getCurrentSession(library));
+  const currentSession = currentSessionData.data.items.wifi_v1[0].session_id;
+  const endDate = currentSessionData.data.items.wifi_v1[0].servertime;
+
+  var startSession = await queryServer(getSessionStart(library, currentSession));
+  const startDate = startSession.data.items.wifi_v1[0].servertime;
+
+  var p = document.createElement('p');
+  p.innerHTML = `Displaying data for current session: <code>${currentSession}</code>, from ${startDate} to ${endDate}`;
+  el.appendChild(p);
+
+  var page = 1;
+  var data = await queryServer(getSessionData(library, currentSession, page));
+  var wifi = data.data.items.wifi_v1.slice(); // make a copy of the data
+
+  while (data.data.items.wifi_v1.length !== 0) {
+    page += 1;
+    data = await queryServer(getSessionData(library, currentSession, page));
+    wifi = Array.prototype.concat(wifi, data.data.items.wifi_v1.slice());
+  }
 
   var last_seen = -1;
   const seen_devices = wifi.reduce((accum, val) => {
